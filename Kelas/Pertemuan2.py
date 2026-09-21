502f35bbf8c1a5eaf4573c2a3838f9f05144f3f3
Rafif FM <rafiffathim@gmail.com>
2026-09-22 00:09:52 +0800

    file python selesai

---
+++ Kelas/Pertemuan2.py
@@ -0,0 +1,111 @@
+print ("Hello, World!")
+
+print ("---------------------")
+
+a = 5
+b = 10
+c = a + b
+print(c)
+
+print ("---------------------")
+
+a = 1.53
+b = 7.2
+c = a + b
+print(a + b)
+
+print ("---------------------")
+
+nama = "Larry"
+hobi = "ngoding"
+print(nama)
+print(hobi)
+
+print ("---------------------")
+
+print('''Halo saya Rendy
+Saya suka belajar coding
+Hari ini saya belajar bahasa pemrograman Python''')
+
+print ("---------------------")
+
+print("aku""rajin""praktikum")
+print("aku"+"rajin")
+
+print ("---------------------")
+
+# Membuat String
+teks = "Rahmat Flowchart"
+#menampilkan index ke-3
+print(teks[3])
+#menampilkan index ke-3 hingga ke-6
+print(teks[3:6])
+#menampilkan seluruhnya, tetapi dimulai dari index ke-7
+print(teks[7:])
+#menampilkan hanya sampai index ke-6
+print(teks[:6])
+
+print ("---------------------")
+
+# Membuat Variabel Boolean
+Hujan = False
+Panas = True
+
+print ("---------------------")
+
+# Membuat List
+mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+print(mata_kuliah[0:1])
+print(mata_kuliah[:2])
+print(mata_kuliah[1:])
+print(mata_kuliah[::2])
+print(mata_kuliah[:-1])
+
+print ("---------------------")
+
+# Membuat Tuple
+data_mahasiswa = ("Rafi","034",True, 3.5)
+print(data_mahasiswa[0])
+print(data_mahasiswa[2])
+
+print ("---------------------")
+
+# Membuat Set
+angka = {1, 2, 3, 4, 4, 5}
+print(angka)
+
+print ("---------------------")
+
+# Membuat Dictionary
+buku = {
+'judul' : 'Atomic Habits',
+'penulis' : 'James Clear',
+'halaman' : 320
+}
+...
+# Mengakses Value dalam Dictionary
+print(buku['judul'])
+print(buku['penulis'])
+print(buku['halaman'])
+
+print ("---------------------")
+
+# Menggunakan Input
+nama = input("Masukan nama anda: ")
+print(nama)
+
+print ("---------------------")
+
+#operator perbandingan
+print(2>3)
+print(2<3)
+print(2>=3)
+print(2<=3)
+print(2==3)
+print(2!=3)
+print(2==2)
+print(2!=2) 
+print(2>=2)
+print(2<=2)
+print(2>2)
+print(2<2)
